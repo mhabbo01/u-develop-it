294496b1b176b722db708de8a06c85a0924dfbeb
Matt Habbo <matthabbo45@gmail.com>
2022-04-20 20:19:20 -0400

    added conection to mysql2 and connection to the db

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //Express middlware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username,
+        user: 'root',
+        //your mysql password
+        password: 'today0311**',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+})
 
 //default response for any other request (Not Found)
 app.use((req, res) => {
